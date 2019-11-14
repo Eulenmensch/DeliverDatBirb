436e67e4d7ce02a2bf158c8f82d7e56565cbbf9c
Yoshi <yoshua.woo@gmail.com>
2019-11-15 00:11:10 +0100

    Created a PlayerController script - still has a long way to go Created Input Assets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Speed;
+
+    private CharacterController CharacterController;
+    private InputMaster Controls;
+    private Vector2 MoveDir;
+    private Vector3 CharacterDir;
+
+    private void Awake()
     {
-        
+        CharacterController = GetComponent<CharacterController>();
+
+        Controls = new InputMaster();
+
+        Controls.Player.Move.started += context => MoveDir = context.ReadValue<Vector2>();
+        Controls.Player.Move.started += context => Move();
+        Controls.Player.Move.canceled += context => MoveDir = Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+    }
+    public void Move()
+    {
+        Debug.Log(CharacterDir);
+        CharacterDir = transform.right * MoveDir.x + transform.forward * MoveDir.y;
+        CharacterDir *= Time.deltaTime;
+        CharacterController.Move(CharacterDir * Speed);
+    }
+
+    private void OnEnable()
+    {
+        Controls.Player.Enable();
+    }
+    private void OnDisable()
+    {
+        Controls.Player.Disable();
     }
 }
