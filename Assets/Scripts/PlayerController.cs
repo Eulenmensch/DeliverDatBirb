917dfeefe34144b69001d84c4cd3959b44fe4423
Yoshua Woo <yoshua.woo@gmail.com>
2019-11-19 22:42:25 +0100

    Modified PlayerController script Player movement now works There is a IsIsometric bool that toggles isometric controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,17 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float Speed;
+    public float GravitationalAcceleration = 9.81f;
+    public bool IsIsometric;
+    public GameObject GroundCheckSphere;
+    public float GroundCheckRadius;
+    public LayerMask GroundLayerMask;
 
     private CharacterController CharacterController;
     private InputMaster Controls;
     private Vector2 MoveDir;
     private Vector3 CharacterDir;
+    private Vector3 VelocityGravitational;
 
     private void Awake()
     {
@@ -18,21 +24,49 @@ public class PlayerController : MonoBehaviour
 
         Controls = new InputMaster();
 
-        Controls.Player.Move.started += context => MoveDir = context.ReadValue<Vector2>();
-        Controls.Player.Move.started += context => Move();
+        Controls.Player.Move.performed += context => MoveDir = context.ReadValue<Vector2>();
         Controls.Player.Move.canceled += context => MoveDir = Vector2.zero;
     }
 
     private void Update()
     {
         Move();
+        ApplyGravity();
+        Rotate();
     }
     public void Move()
     {
-        Debug.Log(CharacterDir);
         CharacterDir = transform.right * MoveDir.x + transform.forward * MoveDir.y;
+        if (IsIsometric)
+        {
+            CharacterDir = Quaternion.AngleAxis(45, Vector3.up) * CharacterDir;
+        }
         CharacterDir *= Time.deltaTime;
         CharacterController.Move(CharacterDir * Speed);
+    }
+
+    void ApplyGravity()
+    {
+        print(IsGrounded());
+        if (IsGrounded() && VelocityGravitational.y < 0)
+        {
+            VelocityGravitational.y = 0;
+        }
+        VelocityGravitational.y -= GravitationalAcceleration * Mathf.Pow(Time.deltaTime, 2);
+        CharacterController.Move(VelocityGravitational);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(GroundCheckSphere.transform.position, GroundCheckRadius, GroundLayerMask);
+    }
+
+    void Rotate()
+    {
+        if (!IsIsometric)
+        {
+            transform.rotation = Camera.main.transform.rotation;
+        }
     }
 
     private void OnEnable()
