2cdb953b8cbb9fd58bb5b23bc70087a3ed157bdf
Yoshua Woo <yoshua.woo@gmail.com>
2020-01-05 13:56:28 +0100

    Upgraded Project to 2019.3.0f3 release candidate. Started working on a new Player Controller Script that doesn't rely on Rigidbody or CharacterController components.

---
+++ Assets/Scripts/PlayerControllerNew.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerNew : MonoBehaviour
+{
+    public float Speed;
+    
+    float HorizontalInput;
+    float VerticalInput;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        HandleMoveInput();
+    }
+
+    void HandleMoveInput(){
+        Vector3 newPosition = transform.position;
+        newPosition.x += HorizontalInput * Time.deltaTime * Speed;
+        newPosition.z += VerticalInput * Time.deltaTime * Speed;
+        transform.position = newPosition;
+    }
+
+    public void GetMoveInput(InputAction.CallbackContext context)
+    {
+        Vector2 inputVector = context.ReadValue<Vector2>();
+        Vector2 rotatedInputVector = new Vector2(
+            inputVector.x * Mathf.Cos(-0.7854f) - inputVector.y * Mathf.Sin(-0.7854f),
+            inputVector.x * Mathf.Sin(-0.7854f) + inputVector.y * Mathf.Cos(-0.7854f)
+        );
+        HorizontalInput = rotatedInputVector.x;
+        VerticalInput = rotatedInputVector.y;
+    }
+}
