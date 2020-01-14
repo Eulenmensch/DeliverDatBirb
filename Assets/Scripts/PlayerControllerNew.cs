b017e5723ab5cd4b55b5ba874afba39f84b28f83
Yoshua Woo <yoshua.woo@gmail.com>
2020-01-14 15:19:17 +0100

    Copied a Script from the Unity docs about the character controller component; Edited the PlayerControllerNew script to adapt it to the Unity docs;
    The latter still does not work properly while the unity docs version does. Going to go forward with the Unity one for now.

--- Assets/Scripts/PlayerControllerNew.cs
+++ Assets/Scripts/PlayerControllerNew.cs
@@ -5,22 +5,60 @@ using UnityEngine.InputSystem;
 
 public class PlayerControllerNew : MonoBehaviour
 {
-    public float Speed;
+    private CharacterController CharacterControllerRef;
 
-    float HorizontalInput;
-    float VerticalInput;
+    public float Speed;
+    public float JumpHeight;
+    public float GravitationalAcceleration;
+
+    private float HorizontalInput;
+    private float VerticalInput;
+    private bool IsReceivingJumpInput;
+
+    private Vector3 MoveDirection;
+    private bool IsJumping;
+
+    private void Start()
+    {
+        CharacterControllerRef = GetComponent<CharacterController>();
+    }
 
     void FixedUpdate()
     {
         HandleMoveInput();
+        HandleJumpInput();
+        ApplyGravity();
+        CharacterControllerRef.Move(MoveDirection * Time.deltaTime);
     }
 
     void HandleMoveInput()
     {
-        Vector3 newPosition = transform.position;
-        newPosition.x += HorizontalInput * Time.deltaTime * Speed;
-        newPosition.z += VerticalInput * Time.deltaTime * Speed;
-        transform.position = newPosition;
+        MoveDirection = new Vector3(HorizontalInput, 0.0f, VerticalInput);
+        MoveDirection *= Speed;
+    }
+
+    void HandleJumpInput()
+    {
+        if (IsJumping && CharacterControllerRef.isGrounded)
+        {
+            IsJumping = false;
+            IsReceivingJumpInput = false;
+        }
+        if (IsReceivingJumpInput)
+        {
+            MoveDirection.y = Mathf.Sqrt(JumpHeight * -2f * -GravitationalAcceleration);
+            // MoveDirection.y = JumpHeight;
+            IsJumping = true;
+        }
+    }
+
+    void ApplyGravity()
+    {
+        // if (CharacterControllerRef.isGrounded && MoveDirection.y < 0)
+        // {
+        //     MoveDirection.y = 0;
+        // }
+        MoveDirection.y -= GravitationalAcceleration * Time.deltaTime;
     }
 
     public void GetMoveInput(InputAction.CallbackContext context)
@@ -32,5 +70,20 @@ public class PlayerControllerNew : MonoBehaviour
         );
         HorizontalInput = rotatedInputVector.x;
         VerticalInput = rotatedInputVector.y;
+    }
+
+    public void GetJumpInput(InputAction.CallbackContext context)
+    {
+        if (CharacterControllerRef.isGrounded)
+        {
+            if (context.phase == InputActionPhase.Performed)
+            {
+                IsReceivingJumpInput = true;
+            }
+        }
+        else
+        {
+            IsReceivingJumpInput = false;
+        }
     }
 }
