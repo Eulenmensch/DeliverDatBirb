2a8b58704f46962dfa16748922fde9d0e2b6f55e
Yoshua Woo <yoshua.woo@gmail.com>
2020-02-04 18:34:53 +0100

    Started Working on a Dropshadow. Will look into a less buggy implementation. (Projection Decal?); Upgraded to 2019.3.0f6

---
+++ Assets/Scripts/DropShadowBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropShadowBehaviour : MonoBehaviour
+{
+    public float YOffset;
+    public LayerMask Mask;
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.parent.position, Vector3.down, out hit, Mathf.Infinity, Mask))
+        {
+            transform.position = new Vector3(transform.position.x, hit.point.y + YOffset, transform.position.z);
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+            Debug.DrawRay(transform.position, hit.normal, Color.green);
+        }
+    }
+}
